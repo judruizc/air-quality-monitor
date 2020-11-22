f4af0950cc1d08082190c4bffd901ce97eed1956
Juan David Ruiz <juan.ruiz@ammp.io>
2020-11-22 20:56:48 +0100

    fixes

--- read_air_sensor.py
+++ read_air_sensor.py
@@ -11,13 +11,6 @@ app = Flask(__name__)
 # turn_off_usb_cmd = 'sudo /home/pi/juan/uhubctl/uhubctl -l 1-1 -a off'
 # turn_on_usb_cmd = 'sudo /home/pi/juan/uhubctl/uhubctl -l 1-1 -a on'
 
-@scheduler.task('cron', id='read_scheduler', day='*', hour='*', minute='*/1')
-def read():
-    data = get_data()
-    print(f"Air pollution - Raw: {data}")
-    data_human = conv_aqi(data[0], data[1])
-    print(f"Air pollution - human: {data_human}")
-    write_to_db(data_human[0], data_human[1])
 
 def get_data():
     sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
@@ -33,15 +26,27 @@ def conv_aqi(pmt_2_5, pmt_10):
     aqi_10 = aqi.to_iaqi(aqi.POLLUTANT_PM10, str(pmt_10))
     return aqi_2_5, aqi_10
 
+
 def write_to_db(pm_2_5, pm_10):
+    print(f"PM2.5: {pm_2_5}, PM10: {pm_10}")
     data_write = {'pm_2_5': pm_2_5,
                   'pm_10': pm_10
                   }
     write_points(measurement='air_pollution', sensor_location='Wintergarten', **data_write)
+
+
+@scheduler.task('cron', id='read_scheduler', day='*', hour='*', minute='*/1')
+def read():
+    print("Attempt to read data..")
+    data = get_data()
+    print(f"Air pollution - Raw: {data}")
+    data_human = conv_aqi(data[0], data[1])
+    print(f"Air pollution - human: {data_human}")
+    write_to_db(data_human[0], data_human[1])
+
 
 scheduler.init_app(app)
 scheduler.start()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=False)
-
